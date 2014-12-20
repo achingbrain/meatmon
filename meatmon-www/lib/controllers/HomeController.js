720662afe7261f7f86dfdca8cffdba22910de758
achingbrain <alex@achingbrain.net>
2014-12-20 09:53:40 +0000

    handle not having enough datapoints

--- meatmon-www/lib/controllers/HomeController.js
+++ meatmon-www/lib/controllers/HomeController.js
@@ -13,19 +13,23 @@ HomeController.prototype.get = function(request, response) {
 
   this._internalTemperatureRepository.findAll(function(error, result) {
 
-    var startTemp = result[result.length - sampleSize].celsius
-    var startTime = new Date(result[result.length - sampleSize].date)
-    var currentTemp = result[result.length - 1].celsius
-    var currentTime = new Date(result[result.length - 1].date)
-    var seconds = (currentTime.getTime() - startTime.getTime()) / 1000
-    var temperatureDiff = currentTemp - startTemp
-    var increasePerSeconds = temperatureDiff/seconds
-    var requiredDiff = targetTemperature - currentTemp
-    var readyInSeconds = requiredDiff/increasePerSeconds
-    var ready = new Date(Date.now() + (readyInSeconds * 1000))
+    if(result.length < sampleSize) {
+      ready = "Not enough data yet.."
+    } else {
+      var startTemp = result[result.length - sampleSize].celsius
+      var startTime = new Date(result[result.length - sampleSize].date)
+      var currentTemp = result[result.length - 1].celsius
+      var currentTime = new Date(result[result.length - 1].date)
+      var seconds = (currentTime.getTime() - startTime.getTime()) / 1000
+      var temperatureDiff = currentTemp - startTemp
+      var increasePerSeconds = temperatureDiff/seconds
+      var requiredDiff = targetTemperature - currentTemp
+      var readyInSeconds = requiredDiff/increasePerSeconds
+      var ready = new Date(Date.now() + (readyInSeconds * 1000))
 
-    if(ready.getTime() < Date.now()) {
-      ready = "Now!"
+      if(ready.getTime() < Date.now()) {
+        ready = "Now!"
+      }
     }
 
     response.render('home', {
